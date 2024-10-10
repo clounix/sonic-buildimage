eef16706e2a5712ba43901703e74c97ff0e0a16b
Jianjun Zhao <zhaojj@clounix.com>
2024-10-10 11:18:19 +0800

    fix<build>switch debian mirror to debian.org and fix build issue

--- scripts/build_mirror_config.sh
+++ scripts/build_mirror_config.sh
@@ -5,14 +5,17 @@ CONFIG_PATH=$1
 export ARCHITECTURE=$2
 export DISTRIBUTION=$3
 
-DEFAULT_MIRROR_URL_PREFIX=http://packages.trafficmanager.net
+#DEFAULT_MIRROR_URL_PREFIX=http://packages.trafficmanager.net
+DEFAULT_MIRROR_URL_PREFIX=http://deb.debian.org
 MIRROR_VERSION_FILE=
 [[ "$SONIC_VERSION_CONTROL_COMPONENTS" == *deb* || $SONIC_VERSION_CONTROL_COMPONENTS == *all* ]] && MIRROR_VERSION_FILE=files/build/versions/default/versions-mirror && MIRROR_SNAPSHOT=y
 [ -f target/versions/default/versions-mirror ] && MIRROR_VERSION_FILE=target/versions/default/versions-mirror
 
 # The default mirror urls
-DEFAULT_MIRROR_URLS=http://debian-archive.trafficmanager.net/debian/
-DEFAULT_MIRROR_SECURITY_URLS=http://debian-archive.trafficmanager.net/debian-security/
+#DEFAULT_MIRROR_URLS=http://debian-archive.trafficmanager.net/debian/
+#DEFAULT_MIRROR_SECURITY_URLS=http://debian-archive.trafficmanager.net/debian-security/
+DEFAULT_MIRROR_URLS=http://deb.debian.org/debian/
+DEFAULT_MIRROR_SECURITY_URLS=http://deb.debian.org/debian-security/
 
 
 # The debian-archive.trafficmanager.net does not support armhf, use debian.org instead
