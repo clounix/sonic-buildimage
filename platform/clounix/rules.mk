f26529f61378d7ebe021a938c8f094047bc1ce4d
wangjingwu <jingwu.wang@phoenixcompany.cn>
2026-03-05 09:48:30 +0800

    add sonic-platform-modules-flines platform

--- platform/clounix/rules.mk
+++ platform/clounix/rules.mk
@@ -1,6 +1,7 @@
 include $(PLATFORM_PATH)/sai.mk
 include $(PLATFORM_PATH)/clounix-modules.mk
 include $(PLATFORM_PATH)/platform-modules-clounix.mk
+include $(PLATFORM_PATH)/platform-modules-flines.mk
 include $(PLATFORM_PATH)/docker-syncd-clounix.mk
 include $(PLATFORM_PATH)/docker-syncd-clounix-rpc.mk
 include $(PLATFORM_PATH)/one-image.mk
