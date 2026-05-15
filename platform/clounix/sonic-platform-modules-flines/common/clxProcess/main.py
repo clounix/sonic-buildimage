bb22a86fbf6f9490228b8eb4274c7f973f9de449
chendamin <damin.chen@phoenixcompany.cn>
2026-05-15 17:39:56 +0800

    撤销reset修改

--- platform/clounix/sonic-platform-modules-flines/common/clxProcess/main.py
+++ platform/clounix/sonic-platform-modules-flines/common/clxProcess/main.py
@@ -111,7 +111,7 @@ def deviceInit():
         path = SFP_PATH  + '/eth' + str(x+1) + '/reset'
         result = common.readFile(path)
         if result != 'NA':
-            result = common.writeFile(path, "1")
+            result = common.writeFile(path, "0")
 
     # Set QSFP power enable  and high power mode  the present signal
     for x in range(0, sfp_num):
