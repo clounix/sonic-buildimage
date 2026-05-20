38c0c7cc6c5acb09cf2aaffd03a369cf98d53cd5
chendamin <damin.chen@phoenixcompany.cn>
2026-05-20 14:50:24 +0800

    优化fan_led的点灯逻辑

--- platform/clounix/sonic-platform-modules-flines/common/sonic_platform/fan_drawer.py
+++ platform/clounix/sonic-platform-modules-flines/common/sonic_platform/fan_drawer.py
@@ -38,5 +38,32 @@ class FanDrawer(PddfFanDrawer):
             self.STATUS_LED_COLOR_OFF   : "off"
         }
         led_device_name = "FAN_LED"
-        result, msg = self.pddf_obj.set_system_led_color(led_device_name, color_dict[color])
-        return (result)
+        present_fans = 0
+        try:
+            num_fantrays = self.pddf_obj.data['PLATFORM']['num_fantrays'] if 'num_fantrays' in self.pddf_obj.data['PLATFORM'] else 5
+            num_fans_pertray = self.pddf_obj.data['PLATFORM']['num_fans_pertray'] if 'num_fans_pertray' in self.pddf_obj.data['PLATFORM'] else 2
+            total_fans = num_fantrays * num_fans_pertray
+            for dev_name in self.pddf_obj.data.keys():
+                if dev_name == 'PLATFORM':
+                    continue
+                dev_type = self.pddf_obj.get_device_type(dev_name)
+                if dev_type == 'FAN':
+                   for fan_num in range(1, total_fans + 1):
+                       attr_name = f"fan{fan_num}_present"
+                       output = self.pddf_obj.get_attr_name_output(dev_name, attr_name)
+                       if output and output['status'].rstrip() == '1':
+                           present_fans += 1
+            if present_fans > 8 :
+                color = self.STATUS_LED_COLOR_GREEN
+            elif present_fans > 6:
+                color = self.STATUS_LED_COLOR_AMBER
+            else:
+                color = self.STATUS_LED_COLOR_RED
+        except Exception as e:
+            print(f"set_status_led Exception: {e}.")
+        finally:
+            old_color = self.get_status_led()
+            if old_color == color_dict[color]:
+                return True
+            result, msg = self.pddf_obj.set_system_led_color(led_device_name, color_dict[color])
+            return (result)
