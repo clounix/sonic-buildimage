b6ab22fa8f3bb1c662c0ed448ff8b5a981f32264
chendamin <damin.chen@phoenixcompany.cn>
2026-06-15 11:21:27 +0800

    优化pci引用计数释放

--- platform/clounix/sonic-platform-modules-flines/ds410g-48y8c/modules/clounix_fpga_sysfs.c
+++ platform/clounix/sonic-platform-modules-flines/ds410g-48y8c/modules/clounix_fpga_sysfs.c
@@ -278,12 +278,12 @@ static int __init clounix_fpga_sysfs_init(void)
     if(pdev) {
         err = sysfs_create_group(&pdev->dev.kobj,&fpga_attribute_group);
         if (err) {
-            pci_dev_put(pdev);
             printk(KERN_ERR  "sysfs_create_file error status %d\n", err);
         }
+        pci_dev_put(pdev);
     }else {
         printk(KERN_ERR "no fpga device vendorid:0x%x  deviceid:0x%x\n", FPGA_VENDOR_ID,FPGA_DEVICE_ID);
-        return -1;
+        return -ENODEV;
     }
     return err;
 }
